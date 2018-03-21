18068d97c8eeb637cd29b0cba289e4f6e9d38964
gold-developer <gold.denis@outlook.com>
2018-03-20 22:26:10 -0700

    issue fix

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Navbar, Button } from "react-bootstrap";
+import "./App.css";
+
+class App extends React.Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem("isLoggedIn") === "false") {
+      renewSession();
+    }
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth;
+
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">React Auth0</a>
+            </Navbar.Brand>
+            {!isAuthenticated() && (
+              <Button
+                id="qsLoginBtn"
+                bsStyle="primary"
+                className="btn-margin"
+                onClick={this.login.bind(this)}
+              >
+                Log In
+              </Button>
+            )}
+            {isAuthenticated() && (
+              <>
+                <Button
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.goTo.bind(this, "home")}
+                >
+                  Home
+                </Button>
+                <Button
+                  id="qsLogoutBtn"
+                  bsStyle="primary"
+                  className="btn-margin"
+                  onClick={this.logout.bind(this)}
+                >
+                  Log Out
+                </Button>
+              </>
+            )}
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
